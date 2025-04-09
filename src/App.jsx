7fac041c6e564919e71955437bb35d22d427deca
jasonnguyen11141 <nguy1442@msu.edu>
2025-04-09 22:21:40 +0000

    started home page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import Navbar from "./components/Navbar/Navbar"
 import Play from "./pages/Play";
 import Collection from "./pages/Collection";
+import Home from "./pages/Home";
 import './App.css'
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+const Home = () => {
+    return (
+    <h1>guess that 'mon</h1>
+    );
+  };
+  
+  export default Home;
+  
