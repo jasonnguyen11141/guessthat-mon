89505bf2cdbdb9dc3d561462dab2615a8229cb41
jasonnguyen11141 <nguy1442@msu.edu>
2025-04-10 21:34:55 +0000

    completed home page, got rid of nav on home.

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,35 @@ import PokemonData from "./components/PokemonData"
 import Play from "./pages/Play"
 import Collection from "./pages/Collection"
 import './App.css'
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Routes, useLocation } from 'react-router-dom'
 import { Navigate } from 'react-router-dom';
 import React from 'react'
 
 
-function App() {
+function AppContent() {
+  const location = useLocation();
+  const hideNavbar = location.pathname === '/';
 
   return (
-    <Router>
-      {<Navbar />}
+    <>
+      {!hideNavbar && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/play" element={<Play />} />
         <Route path="/collection" element={<Collection />} />
-        <Route path="*" element={<Navigate to="/" />} /> {}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AppContent />
     </Router>
   );
 }
+
 
 export default App;
